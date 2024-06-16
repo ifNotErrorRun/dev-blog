d4ee1939cfdbaaa1312e9b836c7ee5789f1bc3dc
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-06-16 21:14:57 +0900

    build: init project (npm -> pnpm)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP, Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/lib/theme/ThemeProvider";
-
-const notoJp = Noto_Sans_JP({ subsets: ["latin"] });
-const notoKr = Noto_Sans_KR({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Chanho.dev",
@@ -18,16 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={notoKr.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
